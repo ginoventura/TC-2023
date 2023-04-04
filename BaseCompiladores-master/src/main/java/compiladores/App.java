05ad80f661c7525b34debf3acfe3dc59ce5d55e3
ginoventura <ginoventura12@gmail.com>
2023-04-04 19:29:01 -0300

    Commit

--- BaseCompiladores-master/src/main/java/compiladores/App.java
+++ BaseCompiladores-master/src/main/java/compiladores/App.java
@@ -1,11 +1,10 @@
 package compiladores;
 
-import org.antlr.v4.runtime.tree.ParseTree;
+// import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
-// Un comentario
 // Las diferentes entradas se explicaran oportunamente
 public class App {
     public static void main(String[] args) throws Exception {
